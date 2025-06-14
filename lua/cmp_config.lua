b4da6693f6881388dcec12e4ad487141ea87956e
MysterJacob <40712909+MysterJacob@users.noreply.github.com>
2025-06-14 16:07:51 +0200

    snippets

--- lua/cmp_config.lua
+++ lua/cmp_config.lua
@@ -1,30 +1,39 @@
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 local select_opts = { behavior = cmp.SelectBehavior.Select }
+
 cmp.setup({
   snippet = {
     expand = function(args)
-      local luasnip = require("luasnip")
       luasnip.lsp_expand(args.body)
     end
   },
+
   sources = {
     { name = 'path' },
+    { name = 'luasnip',       keyword_length = 2 },
     { name = 'nvim_lsp',      keyword_length = 1 },
     { name = 'lsp_signature', keyword_length = 1 },
     { name = 'buffer',        keyword_length = 3 },
-    { name = 'luasnip',       keyword_length = 2 },
   },
-  window = {
-    documentation = cmp.config.window.bordered()
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.kind,
+    }
   },
   formatting = {
     fields = { 'menu', 'abbr', 'kind' },
     format = function(entry, item)
       local menu_icon = {
-        nvim_lsp = 'λ',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫',
+        nvim_lsp = '(lsp)',
+        luasnip = '(snip)',
+        buffer = '(buffer)',
+        lsp_signature = '(signature)',
+        path = '(path)',
       }
 
       item.menu = menu_icon[entry.source.name]
@@ -44,25 +53,27 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, { 'i', 's' }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
+        cmp.select_next_item()
+      elseif luasnip.locally_jumpable(1) then
+        luasnip.jump(1)
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+  },
+  window = {
+    documentation = cmp.config.window.bordered()
   },
 })
---
+++ lua/snip.lua
@@ -0,0 +1,4 @@
+local ls = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load({ paths = { "../snips" } })
+require("luasnip.loaders.from_snipmate").lazy_load()
+
