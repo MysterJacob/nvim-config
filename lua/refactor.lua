4fe7b577b36c646e918b39f06d36602b5ec303e1
MysterJacob <40712909+MysterJacob@users.noreply.github.com>
2026-01-28 19:30:46 +0100

    - Projects.nvim + Refactor.nvim

---
+++ lua/refactor.lua
@@ -0,0 +1,31 @@
+require('refactoring').setup({
+  prompt_func_return_type = {
+    go = false,
+    java = false,
+
+    cpp = false,
+    c = false,
+    h = false,
+    hpp = false,
+    cxx = false,
+  },
+  prompt_func_param_type = {
+    go = false,
+    java = false,
+
+    cpp = false,
+    c = false,
+    h = false,
+    hpp = false,
+    cxx = false,
+  },
+  printf_statements = {},
+  print_var_statements = {},
+  show_success_message = false,
+})
+require("telescope").load_extension("refactoring")
+vim.keymap.set(
+  { "n", "x" },
+  "<leader>rr",
+  function() require('telescope').extensions.refactoring.refactors() end
+)
